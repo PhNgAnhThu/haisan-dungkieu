1947066d19a6b4c307bcd56a18036c7fa908afdd
PhNgAnhThu <anhthuphng05@gmail.com>
2026-01-12 15:46:56 +0700

    100%

--- firebase.js
+++ firebase.js
@@ -1,6 +1,6 @@
-// firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+// import thêm auth, storage nếu cần, nhưng 2 cái trên là chính
 
 const firebaseConfig = {
     apiKey: "AIzaSyDD_3xrt1ryUxzLOBrJFCZcZRpMRYAApVc",
@@ -12,6 +12,7 @@ const firebaseConfig = {
     measurementId: "G-MDWZNKB1ZT"
 };
 
+// Khởi tạo app bình thường, KHÔNG dùng persistence nữa
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
